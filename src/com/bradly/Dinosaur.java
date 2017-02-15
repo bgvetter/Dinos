361927311080e4f9abbb1199e7257428179d2bfd
sylentbv <one@nowhere.com>
2017-02-15 17:22:32 -0600

    New dino project

---
+++ src/com/bradly/Dinosaur.java
@@ -0,0 +1,12 @@
+package com.bradly;
+
+/**
+ * Created by sylentbv on 2/15/2017.
+ */
+public class Dinosaur {
+
+        public void roar(){
+            System.out.println("Grr!!");
+        }
+
+}
